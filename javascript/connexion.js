bb8893ba3c9df71b2a7d67f7f3898be1aee91bc1
Silix <xsilici0m@gmail.com>
2021-12-19 18:30:59 +0100

    Real first web site

---
+++ javascript/connexion.js
@@ -0,0 +1,7 @@
+let e = document.querySelector('.override')
+console.log('-----------')
+console.log(e)
+/*e.addEventListener("click", () => {
+	window.location.href='/html/accueil.html'
+	log(window.location);
+}, false);*/
